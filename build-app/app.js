d7984e70b68cbf745855988cd759cea18afd8923
abdelhak3192 <148243827+abdelhak3192@users.noreply.github.com>
2025-02-25 11:39:36 +0100

    Update app.js

--- build-app/app.js
+++ build-app/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Simple app for the Building Applications Lab!\n');
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT||3080, function () {
   console.log('Simple app for the Building Applications Lab!');
 });
 
