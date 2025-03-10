7e18fe29ce5aa7d7643640dce7519a02e24a0bcc
Abdelhak Benmazouza <abdelhak.benmazouza@ibm.com>
2025-03-10 10:53:59 +0100

    fix app js

--- build-app/app.js
+++ build-app/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Simple app for the Building Applications Lab!\n');
 });
 
-app.listen(process.env.PORT||3080, function () {
+app.listen(process.env.PORT||3070, function () {
   console.log('Simple app for the Building Applications Lab!');
 });
 
